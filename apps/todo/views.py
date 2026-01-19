463533ca8a05d72be51daec83751659dc6401531
MahdiLatifi <mahdilatifi05@gmail.com>
2026-01-19 20:46:20 +0330

    add back

--- apps/todo/views.py
+++ apps/todo/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from .models import Todo
 
 
 # Create your views here.
 @login_required(login_url='http://127.0.0.1:8000/auth/login')
 def index(request):
-    return render(request, 'index.html')
+    todos = Todo.objects.filter(owner=request.user).order_by('is_complete').order_by('-created_at')
+    return render(request, 'index.html', {'todos': todos})
 
 
 @login_required(login_url='http://127.0.0.1:8000/auth/login')
 def profile(request):
     return render(request, "profile.html")
+
+
+@login_required(login_url='http://127.0.0.1:8000/auth/login')
+def add_todo(request):
+    print(request.POST)
+    if request.method == "POST":
+        title = request.POST.get('title')
+        if title:
+            todo = Todo(title=title, owner=request.user)
+            todo.save()
+            return redirect('/')
+        return JsonResponse({'status': 'fail'})
--- statics/js/app.js
+++ statics/js/app.js
@@ -12,12 +12,25 @@ async function undoTodo(todo_id) {
     todo.innerHTML = "<h3>todo " + todo_id + "</h3>\n" + "<button class=\"delete-btn\" type=\"button\" onclick=\"deleteTodo(" + todo_id + ")\">🔘</button>"
 }
 
-async function showChangeName(){
+async function showChangeName() {
     document.getElementById("update-form").classList.remove("hidden")
     document.getElementById("content").classList.add("hidden")
 }
 
-async function showUserDetail(){
+async function showUserDetail() {
     document.getElementById("update-form").classList.add("hidden")
     document.getElementById("content").classList.remove("hidden")
+}
+
+async function addTodo() {
+    let title = document.getElementById('title').value;
+
+    let ele = document.getElementById("content")
+    const todo_id = 3
+    ele.innerHTML = "<div class=\"todo\" id=\"todo-1\">\n" + "<h3>" +
+        title +
+        "</h3>\n" + "<button class=\"delete-btn\" type=\"button\" onclick=\"deleteTodo(" +
+        todo_id +
+        ")\">🔘</button>\n" + " </div>" +
+        ele.innerHTML
 }
