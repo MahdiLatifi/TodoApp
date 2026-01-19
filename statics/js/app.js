695695a2c3d29c9526b650b4fa1efcfcb522a73b
MahdiLatifi <mahdilatifi05@gmail.com>
2026-01-20 00:14:45 +0330

    fix undo todo

--- statics/js/app.js
+++ statics/js/app.js
@@ -1,9 +1,3 @@
-async function undoTodo(todo_id) {
-    let todo = document.getElementById('todo-' + todo_id)
-    todo.classList.remove('deleted-todo')
-    todo.innerHTML = "<h3>todo " + todo_id + "</h3>\n" + "<button class=\"delete-btn\" type=\"button\" onclick=\"deleteTodo(" + todo_id + ")\">🔘</button>"
-}
-
 async function showChangeName() {
     document.getElementById("update-form").classList.remove("hidden")
     document.getElementById("content").classList.add("hidden")
