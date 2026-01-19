15265afdcb0b2326d8e1a70f8b90e559e9e8de29
MahdiLatifi <mahdilatifi05@gmail.com>
2026-01-19 17:20:15 +0330

    make back btn for update details

--- statics/js/app.js
+++ statics/js/app.js
@@ -11,3 +11,13 @@ async function undoTodo(todo_id) {
     todo.classList.remove('deleted-todo')
     todo.innerHTML = "<h3>todo " + todo_id + "</h3>\n" + "<button class=\"delete-btn\" type=\"button\" onclick=\"deleteTodo(" + todo_id + ")\">🔘</button>"
 }
+
+async function showChangeName(){
+    document.getElementById("update-form").classList.remove("hidden")
+    document.getElementById("content").classList.add("hidden")
+}
+
+async function showUserDetail(){
+    document.getElementById("update-form").classList.add("hidden")
+    document.getElementById("content").classList.remove("hidden")
+}
