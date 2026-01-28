d82b7a74a80176ee7398a8302db543bc3d5aa655
MahdiLatifi <mahdilatifi05@gmail.com>
2026-01-28 18:14:11 +0330

    make open to see password with js

--- statics/js/app.js
+++ statics/js/app.js
@@ -1,3 +1,15 @@
+function changeEyeToOpen() {
+    document.getElementById("id_password").setAttribute("type", "text");
+    document.getElementById("closeEye").classList.add("hidden");
+    document.getElementById("openEye").classList.remove("hidden");
+}
+
+function changeEyeToClose() {
+    document.getElementById("id_password").setAttribute("type", "password");
+    document.getElementById("openEye").classList.add("hidden");
+    document.getElementById("closeEye").classList.remove("hidden");
+}
+
 async function showChangeName() {
     document.getElementById("update-form").classList.remove("hidden")
     document.getElementById("content").classList.add("hidden")
